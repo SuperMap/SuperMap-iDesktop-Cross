50f315a00c6426ccc6d0f600d1991f262ef0a96d
lixiaoyao <liwenfa@supermap.com>
2017-11-02 12:50:24 +0800

    图层分组缺陷回归——李文发

--- MapView/src/main/java/com/supermap/desktop/CtrlAction/LayerSetting/CtrlActionUnLayerGroup.java
+++ MapView/src/main/java/com/supermap/desktop/CtrlAction/LayerSetting/CtrlActionUnLayerGroup.java
@@ -9,9 +9,7 @@ import com.supermap.desktop.ui.UICommonToolkit;
 import com.supermap.desktop.ui.controls.LayersTree;
 import com.supermap.desktop.ui.controls.NodeDataType;
 import com.supermap.desktop.ui.controls.TreeNodeData;
-import com.supermap.mapping.Layer;
 import com.supermap.mapping.LayerGroup;
-import com.supermap.mapping.Map;
 
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
@@ -21,7 +19,7 @@ import javax.swing.tree.TreePath;
  */
 public class CtrlActionUnLayerGroup extends CtrlAction {
 //	private TreeNodeData selectedNodeData = null;
-	private TreeNodeData selectedNodeDatas[] =null;
+	private LayerGroup selectedLayerGroup[] =null;
 
 	public CtrlActionUnLayerGroup(IBaseItem caller, IForm formClass) {
 		super(caller, formClass);
@@ -30,26 +28,26 @@ public class CtrlActionUnLayerGroup extends CtrlAction {
 	@Override
 	public void run() {
 		IForm iForm = Application.getActiveApplication().getActiveForm();
-		if (this.selectedNodeDatas != null && iForm != null && iForm instanceof FormMap) {
+		if (this.selectedLayerGroup != null && iForm != null && iForm instanceof FormMap) {
 //			LayersTree layersTree = UICommonToolkit.getLayersManager().getLayersTree();
 //			int originSelectedRowIndex = layersTree.getRowForPath(layersTree.getSelectionPath());
 			FormMap formMap = (FormMap) iForm;
-			for (int i=0;i<this.selectedNodeDatas.length;i++) {
-				LayerGroup layerGroup = (LayerGroup) this.selectedNodeDatas[i].getData();
+			for (int i = 0; i<this.selectedLayerGroup.length; i++) {
+				LayerGroup layerGroup = this.selectedLayerGroup[i];
 				layerGroup.ungroup();
-				formMap.getMapControl().getMap().refresh();
 			}
-			System.out.println(formMap.getMapControl().getMap().getLayers().getCount());
-			Map map = formMap.getMapControl().getMap();
-			try {
-				for (int i = 0; i < map.getLayers().getCount(); i++) {
-					Layer layer = map.getLayers().get(i);
-				}
-			} catch (Exception ex) {
-				System.out.println("Current count is:");
-				System.out.println(map.getLayers().getCount());
-				Application.getActiveApplication().getOutput().output(ex);
-			}
+			formMap.getMapControl().getMap().refresh();
+//			System.out.println(formMap.getMapControl().getMap().getLayers().getCount());
+//			Map map = formMap.getMapControl().getMap();
+//			try {
+//				for (int i = 0; i < map.getLayers().getCount(); i++) {
+//					Layer layer = map.getLayers().get(i);
+//				}
+//			} catch (Exception ex) {
+//				System.out.println("Current count is:");
+//				System.out.println(map.getLayers().getCount());
+//				Application.getActiveApplication().getOutput().output(ex);
+//			}
 		}
 	}
 
@@ -59,7 +57,7 @@ public class CtrlActionUnLayerGroup extends CtrlAction {
 		LayersTree layersTree = UICommonToolkit.getLayersManager().getLayersTree();
 		if (layersTree != null &&layersTree.getSelectionPaths()!=null) {
 			TreePath treePath[]= layersTree.getSelectionPaths();
-			this.selectedNodeDatas=new TreeNodeData[treePath.length];
+			this.selectedLayerGroup =new LayerGroup[treePath.length];
 
 			for (int i=0;i<treePath.length;i++){
 				DefaultMutableTreeNode selectedTreeNode = (DefaultMutableTreeNode)treePath[i].getLastPathComponent();
@@ -67,7 +65,7 @@ public class CtrlActionUnLayerGroup extends CtrlAction {
 				if (selectedTreeNodeData.getType() != NodeDataType.LAYER_GROUP && selectedTreeNodeData.getType()!=NodeDataType.LAYER_SNAPSHOT) {
 					break;
 				}else{
-					this.selectedNodeDatas[i]=selectedTreeNodeData;
+					this.selectedLayerGroup[i]=(LayerGroup)selectedTreeNodeData.getData();
 					if (i==treePath.length-1){
 						enable=true;
 					}
@@ -78,7 +76,7 @@ public class CtrlActionUnLayerGroup extends CtrlAction {
 
 		}
 		if (!enable) {
-			this.selectedNodeDatas = null;
+			this.selectedLayerGroup = null;
 		}
 		return enable;
 	}
