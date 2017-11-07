83801d539b3b40798ce4623bc71792fd1dda1408
mapcaoliping <caoliping@supermap.com>
2017-11-07 17:04:55 +0800

    去除国际化方案中不必要的判断

--- Core/src/main/java/com/supermap/desktop/WorkEnvironmentManager.java
+++ Core/src/main/java/com/supermap/desktop/WorkEnvironmentManager.java
@@ -1,6 +1,5 @@
 package com.supermap.desktop;
 
-import com.alibaba.fastjson.JSONArray;
 import com.supermap.desktop.properties.Properties;
 import com.supermap.desktop.utilities.PathUtilities;
 import com.supermap.desktop.utilities.XmlUtilities;
@@ -28,25 +27,12 @@ public class WorkEnvironmentManager {
 		Boolean result = false;
 		String name = Properties.getLocale().getLanguage() + "_" + Properties.getLocale().getCountry();
 		try {
-			// 遍历所有工作环境
-			String desktopCrossStartArgs = System.getProperty("DesktopCrossStartArgs");
-			JSONArray fileLists = (JSONArray) JSONArray.parse(desktopCrossStartArgs);
-			if (fileLists != null && fileLists.size() != 0) {
-				String language = (String) fileLists.get(0);
-				if (language.equals("zh_CN")) {
-					name = "Default";
-				}
-				if (language.equals("en_US")) {
-					name = "Default_EN_US";
-				}
-			} else {
-				String language = name;
-				if (language.equals("zh_CN")) {
-					name = "Default";
-				}
-				if (language.equals("en_US")) {
-					name = "Default_EN_US";
-				}
+			String language = name;
+			if (language.equals("zh_CN")) {
+				name = "Default";
+			}
+			if (language.equals("en_US")) {
+				name = "Default_EN_US";
 			}
 			String workEnvironmentPath = PathUtilities.getFullPathName(_XMLTag.g_FolderWorkEnvironment, true);
 			File file = new File(workEnvironmentPath);
