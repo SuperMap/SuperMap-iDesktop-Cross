b374fb4cc809e6edfbecac2ea986497feef1a1fb
highsad <wuxiaobo@supermap.com>
2017-11-14 08:46:45 +0800

    1. 添加工作流类型管理的详细备注说明。 2. 移除无用的 Process.xml 文件。

--- Process/src/main/java/com/supermap/desktop/process/virtual/VirtualType.java
+++ Process/src/main/java/com/supermap/desktop/process/virtual/VirtualType.java
@@ -3,11 +3,26 @@ package com.supermap.desktop.process.virtual;
 import java.util.HashMap;
 
 /**
+ * 类型管理，用来对类型编码，并可方便的自定义类型，也可简单的根据已有类型组合新的类型。
+ * 同时在后续工作流交互过程中，各种对类型的求交、求并、求等等操作，拥有简洁的代码和良好的性能。
+ * 比如：简单矢量数据集类型 是否包含于 矢量数据集类型；矢量点线数据集 是否部分相交于 简单二维矢量数据集。
+ * 编码规则如下，从 1 开始，分若干个编码区间，每一个编码区间取 8 位编码，比如 0100 0000、1001 0101，同一个类型由一个或多个编码区间组成类型编码。
+ * 不同类型的相同编码区间，相同的编码位置如果都为 1，则认为这两种类型有部分相同的类型特性，比如 假设点线数据集类型是 0000 0011，线面数据集是
+ * 0000 0110，从右往左第二位都是1，表示线数据集类型，如果有功能需求线数据集，则都可以使用这两种数据集类型作为参数。
+ *
+ *类型分原子类型和复杂类型。
+ * 所有原子类型的编码相互之间都没有相同的类型特性，也就是不会有任何两个原子类型的编码位同为 1，也就意味着原子类型的编码永远只有一段，并且
+ * 只有一位编码位为 1，其余七位编码位为 0，同一段最多能获取8个不同的编码，从 0000 0001 开始，到 1000 0000 结束。
+ * 原子类型编码通过 VritualType 提供的静态方法 registerNewUniqueType 直接获取可用的唯一编码。
+ *复杂类型编码，由多个原子类型编码组合而成，将相同 section 的编码按位或得到新的 section 类型编码，比如 section 1 的类型编码 0100 0000 和 1000 0000
+ * 按位或组合而成的新的 section 1 类型编码为 1100 0000。
  * Created by highsad on 2017/11/10.
  */
 public class VirtualType {
 	public static VirtualType OBJECT = new VirtualType();
 	private static int currentSection;
+
+	// 已分配的 section 原子编码，每一段初始化为 0000 0000，分配一个，就将对应的位设置为1，直到 1111 1111，则该 section 分配完毕，新建 section 继续分配
 	private static HashMap<Integer, Byte> registeredSectionsMap = new HashMap<>();
 
 	private HashMap<Integer, Byte> code = new HashMap<>();
@@ -47,6 +62,8 @@ public class VirtualType {
 		}
 
 		byte value = registeredSectionsMap.get(currentSection);
+
+		// 如果当前 section 已经分配完编码，则新建 section，并初始化为 0000 0000
 		if (value == (byte) 0xFF) {
 			currentSection++;
 			registeredSectionsMap.put(currentSection, (byte) 0x00);
@@ -55,9 +72,19 @@ public class VirtualType {
 		return currentSection;
 	}
 
+	/**
+	 * 分配一个可用的类型编码
+	 * @return
+	 */
 	private static byte distributeAvailableCode() {
+
+		// 获取当前 section 已分配原子编码
 		byte sectionCode = registeredSectionsMap.get(currentSection);
+
+		// 加一，就可以得到新的原子类型编码
 		byte typeCode = (byte) (sectionCode & 0xFF + 0x01);
+
+		// 再将新分配的原子编码与已分配的 section 编码按位或，即可得到新的 section 已分配编码
 		sectionCode = (byte) (sectionCode | typeCode);
 		registeredSectionsMap.put(currentSection, sectionCode);
 		return typeCode;
