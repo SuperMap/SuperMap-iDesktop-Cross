00da5d78c1f591dbd0b96b704dbd38d9550bf67c
xiajt <xiajintao@supermap.com>
2017-11-15 16:29:15 +0800

    添加缺少的资源文件

---
+++ Controls/src/main/java/com/supermap/desktop/CtrlAction/CtrlActionSkin2007Blue.java
@@ -0,0 +1,14 @@
+package com.supermap.desktop.CtrlAction;
+
+import com.supermap.desktop.Interface.IBaseItem;
+import com.supermap.desktop.Interface.IForm;
+import com.supermap.desktop.implement.CtrlAction;
+
+/**
+ * @author XiaJT
+ */
+public class CtrlActionSkin2007Blue extends CtrlAction{
+	public CtrlActionSkin2007Blue(IBaseItem caller, IForm formClass) {
+		super(caller, formClass);
+	}
+}
